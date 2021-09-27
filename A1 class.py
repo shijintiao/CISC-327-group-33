eb70eca17a063e4bd8e13d3fb984305e60482c7e
shijintiao <1942839435@qq.com>
2021-09-27 16:13:03 -0400

    add comment of bORs

--- A1 class.py
+++ A1 class.py
@@ -13,6 +13,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     passord = db.Column(db.String(80), unique=True, nullable=False)
     balance = db.Column(db.Integer, unique=True)
+    #0 stands for buyer, 1 stands for seller
     bORs= db.Column(db.Integer, nullable=False)
     products = db.relationship("Product")
     
