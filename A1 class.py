d641779c8be2d671e15f44e3c3c56bf44874d205
17hl111 <71146643+17hl111@users.noreply.github.com>
2021-09-27 20:34:32 -0400

    Add Banking_info class
    Add a class about customer's banking information.

--- A1 class.py
+++ A1 class.py
@@ -32,3 +32,18 @@ class Product(db.Model):
     def __repr__(self):
         return '<Product %r)' % self.productName
     
+class Banking_info(db.Model):
+    #basic attributes
+    id = db.Column(db.Integer, primary_key=True)
+    Banking_name = db.Column(db.String(20),unique=False, nullable=True)
+    Banking_account = db.Column(db.Integer, unique=True)
+    #Banking token is a secret key from bank to customer so the customer won't need Personal identity verification to complete the transcation. 
+    Banking_token = db.Column(db.String(80), unique=True,nullable=True)
+    
+    def __repr__(self):
+        return '<Customer Bank %r)' % self.Banking_name
+    
+    
+    
+    
+    
