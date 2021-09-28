82e8588ccd139db6be58332589758207d2dfed53
oliverguo666 <59981589+oliverguo666@users.noreply.github.com>
2021-09-27 20:10:25 -0400

    Update A1 class.py

--- A1 class.py
+++ A1 class.py
@@ -11,9 +11,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    passord = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=True, nullable=False)
     balance = db.Column(db.Integer, unique=True)
-    #0 stands for buyers, 1 stands for sellers.
     bORs= db.Column(db.Integer, nullable=False)
     products = db.relationship("Product")
     
@@ -27,8 +26,31 @@ class Product(db.Model):
     productName = db.Column(db.String(80), unique=True, nullable=False)
     seller = db.Column(db.Integer, db.ForeignKey("user_id"))
     stock = db.Column(db.Integer, nullable=False)
-    price = db.Column(db.Integer, unique=True, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
         return '<Product %r)' % self.productName
     
+
+# Create Transaction class
+class Transaction(db.Model):
+    # basic attributes
+    id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.String(80), unique=True, nullable=False)
+    address = db.Column(db.String(120), unique=True, nullable=False)
+    order_status = db.Column(db.String(80), nullable=False)
+    complete_time = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<User %r>' % self.name
+
+
+# Create Transaction detail class
+class Transaction_detail(db.Model):
+    # basic attributes
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return '<Transaction %r>' % self.name
