f865637d41737aaa444c036b73342063f123a26d
17hl111 <17hl111@queensu.ca>
2021-10-10 20:53:30 -0400

    Add comments to test code4️⃣
    Add comments to test code to explain how each assert statement test each requirement.

--- qbay_test/test_models.py
+++ qbay_test/test_models.py
@@ -33,23 +33,25 @@ def test_r4_1_Create_product():
     product = Create_product("PP1", "from brand Alienware and it \
       is brand new", last_modified_date, 100, "test0@test.com")
     assert product is not None
-
+    # R4-8: A user cannot create products that have the same title.
     product = Create_product("PP1", "from brand Alienware and it is \
       brand new", last_modified_date, 1000, "17hl111@queensu.ca")
     assert product is None
-
+    # R4-1: The title of the product has to be alphanumeric-only, \
+    # and space allowed only if it is not as prefix and suffix.
     product = Create_product(" P1", "from brand Alienware and it \
       is brand new", last_modified_date, 100, "test0@test.com")
     assert product is None
-
+    # R4-4: Description has to be longer than the product's title.
     product = Create_product("Pppppppppp2", "from ",
                              last_modified_date, 100, "test0@test.com")
     assert product is None
-
+    # R4-5: Price has to be of range [10, 10000].
     product = Create_product("P2", "from brand Alienware and it \
       is brand new", last_modified_date, 9, "test0@test.com")
     assert product is None
-
+    # R4-7: owner_email cannot be empty. The owner of the \
+    # corresponding product must exist in the database.
     product = Create_product("P3", "from brand Alienware and it \
       is brand new", last_modified_date, 100, "")
     assert product is None
