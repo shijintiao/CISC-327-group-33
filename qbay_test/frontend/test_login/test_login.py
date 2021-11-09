43fbfee566a60613719eb223b0e829756af8047c
Yue Fang <fangyue0529@hotmail.com>
2021-11-08 23:05:39 -0500

    Update test cases in creat_product and login
    Now .out files have success information correctly

--- qbay_test/frontend/test_login/test_login.py
+++ qbay_test/frontend/test_login/test_login.py
@@ -5,20 +5,27 @@ import subprocess
 # get expected input/output file
 current_folder = Path(__file__).parent
 
+'''
+There are three test parts in the login test which are the
+input test, output test and function test.
+test_login_1 represents the test of input, test all possibilities
+of input(one test case for each category)
+test_login_2 represents the test of output, test all possibilities
+of out(one test case for each category)
+test_login_3 represents the test of function, to test the function,
+we need to make sure that the user can login.
+'''
 
-# read expected in/out
-expected_in = open(current_folder.joinpath(
-    'test_login.in'))
-expected_out = open(current_folder.joinpath(
-    'test_login.out')).read().replace('\n', '')
 
-print(expected_out)
-
-
-def test_login():
+def test_login_1():
     """capsys -- object created by pytest to
     capture stdout and stderr"""
 
+    # read expected in/out
+    expected_in = open(current_folder.joinpath(
+        'test_login_1.in'))
+    expected_out = open(current_folder.joinpath(
+        'test_login_1.out')).read().replace('\n', '')
     # pip the input
     output = subprocess.run(
         ['python', '-m', 'qbay'],
